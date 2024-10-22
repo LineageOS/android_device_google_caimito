CalyxOS/device_google_caimito

caimito: Set up proprietary files extraction
* The proprietary files lists are generated by comparing stock partition
  contents with AOSP generated partitions.

Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>
Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

caiman/setup-makefiles.sh
@@ -1,0 +1,88 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=caiman
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+export TARGET_ENABLE_CHECKELF=true
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/google/caimito",
+		"hardware/google/av",
+		"hardware/google/gchips",
+		"hardware/google/graphics/common",
+		"hardware/google/interfaces",
+		"hardware/google/pixel",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.google.edgetpu_app_service-V4-ndk | \
+            com.google.edgetpu_vendor_service-V2-ndk)
+            echo "$1-vendor"
+            ;;
+        libprotobuf-cpp-full-21.12)
+            echo "libprotobuf-cpp-full"
+            ;;
+        libprotobuf-cpp-lite-21.12)
+            echo "libprotobuf-cpp-lite"
+            ;;
+        libacryl | \
+            libexynosv4l2) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt"
+write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt"
+write_makefiles "${MY_DIR}/proprietary-files-vendor.txt"
+
+write_rro_package "CarrierConfigOverlay" "com.android.carrierconfig" product
+write_single_product_copy_files "product/etc/apns-conf.xml"
+write_single_product_packages "CarrierConfigOverlay"
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
+
+bash "${ANDROID_ROOT}"/calyx/scripts/pixel/prepare-firmware-makefiles.sh "${DEVICE}" "${ANDROIDMK}" "${BOARDMK}"
+
+# Finish
+write_footers

komodo/setup-makefiles.sh
@@ -1,0 +1,88 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=komodo
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+export TARGET_ENABLE_CHECKELF=true
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/google/caimito",
+		"hardware/google/av",
+		"hardware/google/gchips",
+		"hardware/google/graphics/common",
+		"hardware/google/interfaces",
+		"hardware/google/pixel",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.google.edgetpu_app_service-V4-ndk | \
+            com.google.edgetpu_vendor_service-V2-ndk)
+            echo "$1-vendor"
+            ;;
+        libprotobuf-cpp-full-21.12)
+            echo "libprotobuf-cpp-full"
+            ;;
+        libprotobuf-cpp-lite-21.12)
+            echo "libprotobuf-cpp-lite"
+            ;;
+        libacryl | \
+            libexynosv4l2) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt"
+write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt"
+write_makefiles "${MY_DIR}/proprietary-files-vendor.txt"
+
+write_rro_package "CarrierConfigOverlay" "com.android.carrierconfig" product
+write_single_product_copy_files "product/etc/apns-conf.xml"
+write_single_product_packages "CarrierConfigOverlay"
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
+
+bash "${ANDROID_ROOT}"/calyx/scripts/pixel/prepare-firmware-makefiles.sh "${DEVICE}" "${ANDROIDMK}" "${BOARDMK}"
+
+# Finish
+write_footers

tokay/extract-files.sh
@@ -1,0 +1,131 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=tokay
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+export TARGET_ENABLE_CHECKELF=true
+
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+ONLY_FIRMWARE=
+KANG=
+SECTION=
+CARRIER_SKIP_FILES=()
+VENDOR_SKIP_FILES=()
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
+
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/init/init.modem_logging_control.rc)
+            [ "$2" = "" ] && return 0
+            sed -i 's/ && property:ro.debuggable=0//' "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
+function prepare_firmware() {
+    if [ "${SRC}" != "adb" ]; then
+        bash "${ANDROID_ROOT}"/calyx/scripts/pixel/prepare-firmware.sh "${DEVICE}" "${SRC}"
+    fi
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+if [ -z "${ONLY_FIRMWARE}" ]; then
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+    generate_prop_list_from_image "product.img" "${MY_DIR}/proprietary-files-carriersettings.txt" CARRIER_SKIP_FILES carriersettings
+    extract "${MY_DIR}/proprietary-files-carriersettings.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+    extract_carriersettings
+
+    readarray -t VENDOR_SKIP_FILES < <(cat "${MY_DIR}/skip-files-vendor.txt" | sed -E "/^[[:blank:]]*(#|$)/d")
+    VENDOR_TXT="${MY_DIR}/proprietary-files-vendor.txt"
+    generate_prop_list_from_image "vendor.img" "${VENDOR_TXT}" VENDOR_SKIP_FILES
+
+    # libExynosC2*Dec & libExynosC2*Enc depend on libacryl & libexynosv4l2, which are gnu makefile targets
+    set_disable_checkelf "vendor/lib64/libExynosC2H263Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2H263Enc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2H264Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2H264Enc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2HevcDec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2HevcEnc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Mpeg4Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Mpeg4Enc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Vp8Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Vp8Enc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Vp9Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Vp9Enc.so" "${VENDOR_TXT}"
+
+    set_module_suffix "vendor/lib64/com.google.edgetpu_app_service-V4-ndk.so" "-vendor" "${VENDOR_TXT}"
+    set_module_suffix "vendor/lib64/com.google.edgetpu_vendor_service-V2-ndk.so" "-vendor" "${VENDOR_TXT}"
+
+    extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
+
+if [ -z "${SECTION}" ]; then
+    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
+fi
+
+"${MY_DIR}/setup-makefiles.sh"

tokay/setup-makefiles.sh
@@ -1,0 +1,88 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=tokay
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+export TARGET_ENABLE_CHECKELF=true
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/google/caimito",
+		"hardware/google/av",
+		"hardware/google/gchips",
+		"hardware/google/graphics/common",
+		"hardware/google/interfaces",
+		"hardware/google/pixel",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.google.edgetpu_app_service-V4-ndk | \
+            com.google.edgetpu_vendor_service-V2-ndk)
+            echo "$1-vendor"
+            ;;
+        libprotobuf-cpp-full-21.12)
+            echo "libprotobuf-cpp-full"
+            ;;
+        libprotobuf-cpp-lite-21.12)
+            echo "libprotobuf-cpp-lite"
+            ;;
+        libacryl | \
+            libexynosv4l2) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt"
+write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt"
+write_makefiles "${MY_DIR}/proprietary-files-vendor.txt"
+
+write_rro_package "CarrierConfigOverlay" "com.android.carrierconfig" product
+write_single_product_copy_files "product/etc/apns-conf.xml"
+write_single_product_packages "CarrierConfigOverlay"
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
+
+bash "${ANDROID_ROOT}"/calyx/scripts/pixel/prepare-firmware-makefiles.sh "${DEVICE}" "${ANDROIDMK}" "${BOARDMK}"
+
+# Finish
+write_footers

LineageOS/android_device_google_caimito

caiman/setup-makefiles.sh
@@ -1,0 +1,86 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=caiman
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/google/caimito",
+		"hardware/google/av",
+		"hardware/google/gchips",
+		"hardware/google/graphics/common",
+		"hardware/google/interfaces",
+		"hardware/google/pixel",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.google.edgetpu_app_service-V4-ndk | \
+            com.google.edgetpu_vendor_service-V2-ndk)
+            echo "$1-vendor"
+            ;;
+        libprotobuf-cpp-full-21.12)
+            echo "libprotobuf-cpp-full"
+            ;;
+        libprotobuf-cpp-lite-21.12)
+            echo "libprotobuf-cpp-lite"
+            ;;
+        libacryl | \
+            libexynosv4l2) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt"
+write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt"
+write_makefiles "${MY_DIR}/proprietary-files-vendor.txt"
+
+write_rro_package "CarrierConfigOverlay" "com.android.carrierconfig" product
+write_single_product_copy_files "product/etc/apns-conf.xml"
+write_single_product_packages "CarrierConfigOverlay"
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
+
+bash "${ANDROID_ROOT}"/lineage/scripts/pixel/prepare-firmware-makefiles.sh "${DEVICE}" "${ANDROIDMK}" "${BOARDMK}"
+
+# Finish
+write_footers

komodo/setup-makefiles.sh
@@ -1,0 +1,86 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=komodo
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/google/caimito",
+		"hardware/google/av",
+		"hardware/google/gchips",
+		"hardware/google/graphics/common",
+		"hardware/google/interfaces",
+		"hardware/google/pixel",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.google.edgetpu_app_service-V4-ndk | \
+            com.google.edgetpu_vendor_service-V2-ndk)
+            echo "$1-vendor"
+            ;;
+        libprotobuf-cpp-full-21.12)
+            echo "libprotobuf-cpp-full"
+            ;;
+        libprotobuf-cpp-lite-21.12)
+            echo "libprotobuf-cpp-lite"
+            ;;
+        libacryl | \
+            libexynosv4l2) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt"
+write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt"
+write_makefiles "${MY_DIR}/proprietary-files-vendor.txt"
+
+write_rro_package "CarrierConfigOverlay" "com.android.carrierconfig" product
+write_single_product_copy_files "product/etc/apns-conf.xml"
+write_single_product_packages "CarrierConfigOverlay"
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
+
+bash "${ANDROID_ROOT}"/lineage/scripts/pixel/prepare-firmware-makefiles.sh "${DEVICE}" "${ANDROIDMK}" "${BOARDMK}"
+
+# Finish
+write_footers

tokay/extract-files.sh
@@ -1,0 +1,125 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=tokay
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+ONLY_FIRMWARE=
+KANG=
+SECTION=
+CARRIER_SKIP_FILES=()
+VENDOR_SKIP_FILES=()
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
+
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/init/init.modem_logging_control.rc)
+            [ "$2" = "" ] && return 0
+            sed -i 's/ && property:ro.debuggable=0//' "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
+function prepare_firmware() {
+    if [ "${SRC}" != "adb" ]; then
+        bash "${ANDROID_ROOT}"/lineage/scripts/pixel/prepare-firmware.sh "${DEVICE}" "${SRC}"
+    fi
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+
+if [ -z "${ONLY_FIRMWARE}" ]; then
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+    generate_prop_list_from_image "product.img" "${MY_DIR}/proprietary-files-carriersettings.txt" CARRIER_SKIP_FILES carriersettings
+    extract "${MY_DIR}/proprietary-files-carriersettings.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+    extract_carriersettings
+
+    readarray -t VENDOR_SKIP_FILES < <(cat "${MY_DIR}/skip-files-vendor.txt" | sed -E "/^[[:blank:]]*(#|$)/d")
+    VENDOR_TXT="${MY_DIR}/proprietary-files-vendor.txt"
+    generate_prop_list_from_image "vendor.img" "${VENDOR_TXT}" VENDOR_SKIP_FILES
+
+    # libExynosC2*Dec & libExynosC2*Enc depend on libacryl & libexynosv4l2, which are gnu makefile targets
+    set_disable_checkelf "vendor/lib64/libExynosC2H263Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2H263Enc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2H264Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2H264Enc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2HevcDec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2HevcEnc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Mpeg4Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Mpeg4Enc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Vp8Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Vp8Enc.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Vp9Dec.so" "${VENDOR_TXT}"
+    set_disable_checkelf "vendor/lib64/libExynosC2Vp9Enc.so" "${VENDOR_TXT}"
+
+    set_module_suffix "vendor/lib64/com.google.edgetpu_app_service-V4-ndk.so" "-vendor" "${VENDOR_TXT}"
+    set_module_suffix "vendor/lib64/com.google.edgetpu_vendor_service-V2-ndk.so" "-vendor" "${VENDOR_TXT}"
+
+    extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
+
+if [ -z "${SECTION}" ]; then
+    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
+fi
+
+"${MY_DIR}/setup-makefiles.sh"

tokay/setup-makefiles.sh
@@ -1,0 +1,86 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=tokay
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/google/caimito",
+		"hardware/google/av",
+		"hardware/google/gchips",
+		"hardware/google/graphics/common",
+		"hardware/google/interfaces",
+		"hardware/google/pixel",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.google.edgetpu_app_service-V4-ndk | \
+            com.google.edgetpu_vendor_service-V2-ndk)
+            echo "$1-vendor"
+            ;;
+        libprotobuf-cpp-full-21.12)
+            echo "libprotobuf-cpp-full"
+            ;;
+        libprotobuf-cpp-lite-21.12)
+            echo "libprotobuf-cpp-lite"
+            ;;
+        libacryl | \
+            libexynosv4l2) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt"
+write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt"
+write_makefiles "${MY_DIR}/proprietary-files-vendor.txt"
+
+write_rro_package "CarrierConfigOverlay" "com.android.carrierconfig" product
+write_single_product_copy_files "product/etc/apns-conf.xml"
+write_single_product_packages "CarrierConfigOverlay"
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
+
+bash "${ANDROID_ROOT}"/lineage/scripts/pixel/prepare-firmware-makefiles.sh "${DEVICE}" "${ANDROIDMK}" "${BOARDMK}"
+
+# Finish
+write_footers